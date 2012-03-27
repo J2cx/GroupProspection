808214d4e7010dcc266c0f6252e813b4d4d4a641
J2cx <zhugezheloic@hotmail.com>
2012-03-27 15:33:40 +0200

    1,sql return ArrayList for table/column, its not container;2, server on local and lib sqljdbc on local

--- src/java/com/example/vaadin/MyApplication.java
+++ src/java/com/example/vaadin/MyApplication.java
@@ -9,6 +9,17 @@ package com.example.vaadin;
 import com.vaadin.Application;
 import com.vaadin.ui.*;
 import com.vaadin.data.*;
+import com.vaadin.data.util.BeanItemContainer;
+import java.sql.Array;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.vaadin.ui.Table;
+import java.util.Iterator;
+import java.util.Set;
 /** 
  *
  * @author weizhe.jiao
@@ -22,8 +33,42 @@ public class MyApplication extends Application {
 	Window mainWindow = new Window("MyApplication");
         //Label label = new Label("Hello Vaadin user");
         Label label = new Label( Integer.toString(SQL.Int("select count(id) from prospecteurs")) );
-	mainWindow.addComponent(label);
+        
+        String s=Integer.toString(SQL.Arr("select mail,Pre from prospecteurs").size());
+        System.out.println(s);
+        Label label3 = new Label(s);
+
+
+        Table t = new Table();
+        ArrayList<HashMap> ah = SQL.Arr("select mail,Pre from prospecteurs");
+        BeanItemContainer<HashMap> container = new BeanItemContainer<HashMap>(HashMap.class);
+        for (HashMap o : ah) {
+            container.addBean(o);
+        }
+        // Create a Set with the keys in the HashMap.
+        Set set = ah.get(1).keySet();
+
+        // Iterate over the Set to see what it contains.
+        Iterator iter = set.iterator();
+        while (iter.hasNext())
+        {
+            Object o = iter.next();
+            System.out.println("keySet: " + o.toString());
+            
+        }
+        Object[] array = set.toArray(); 
+        
+        t.setContainerDataSource(container);
+        t.setVisibleColumns(array);
+        
+        
+        mainWindow.addComponent(label3);
+        mainWindow.addComponent(t);
+        
+	//mainWindow.addComponent(label);
 	setMainWindow(mainWindow);
     }
+
+
 
 }
--- src/java/com/example/vaadin/SQL.java
+++ src/java/com/example/vaadin/SQL.java
@@ -1,11 +1,21 @@
 package com.example.vaadin;
 
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.BeanItem;
 import java.sql.*;
-import javax.sql.*;
-import javax.naming.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class SQL {
-
+    
+    /* 
+     * server on da-sisi2 
     private static DataSource dataSource;
 
     static {
@@ -19,10 +29,62 @@ public class SQL {
     public static Connection getConnection()
             throws SQLException {
         return dataSource.getConnection();
-    }
+    }*/
 
+    
+    /* server on local   
+     * lib sqljdbc4.jar
+     */
+     private static Connection  con = null;
+     private final static String url = "jdbc:sqlserver://";
+     private final static String serverName= "da-newdm";
+     private final static String portNumber = "1433";
+     private final static String databaseName= "db_prospection";
+     private final static String userName = "sa";
+     private final static String password = "limubai64";
+     // Informs the driver to use server a side-cursor,
+     // which permits more than one active statement
+     // on a connection.
+     private final static String selectMethod = "cursor";
+
+     private static String getConnectionUrl(){
+          return url+serverName+":"+portNumber+";databaseName="+databaseName+";selectMethod="+selectMethod+";";
+     }
+
+     private static Connection getConnection(){
+          try{
+               Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+               con = DriverManager.getConnection(getConnectionUrl(),userName,password);
+               if(con!=null) System.out.println("Connection Successful!");
+          }catch(Exception e){
+               e.printStackTrace();
+               System.out.println("Error Trace in getConnection() : " + e.getMessage());
+         }
+          return con;
+      }
+    
+    
     /*  returns a boolean value ( true/false [default false] ) */
-    //public static String Bool(String strSQL) {
+    public static Boolean Bool(String strSQL) {
+        Boolean result = false;
+        Statement stmt;
+        ResultSet rs;
+        
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeQuery(strSQL);
+            rs = stmt.getResultSet();
+            if (rs.next()) {
+                result = rs.getBoolean(1);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
     
     /* returns a non-null integer, default 0 */
     public static int Int(String strSQL) {
@@ -47,33 +109,170 @@ public class SQL {
     }
     
     /* returns a non-null string, default '' (empty) */
-    //public static String Str(String strSQL) {
+    public static String Str(String strSQL) {
+        String result = "";
+        Statement stmt;
+        ResultSet rs;
+        
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeQuery(strSQL);
+            rs = stmt.getResultSet();
+            if (rs.next()) {
+                result = rs.getString(1);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
     
     /* executes a command (insert/update/procedure) */
-    //public static void Do(String strSQL) {
+    public static void Do(String strSQL) {
+        Statement stmt;
+        
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeUpdate(strSQL);
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
     
-    /* returns a non-null date (default '31/12/1999 23:59:59' ) */
-    //public static java.sql.Timestamp Dat(String strSQL) {
-    
-    /* returns an array ( recordset -> array ) */
     /*
-     table = new table()
-     table.dataSource = sql.arr("select top 10 prenom, nom from users")
-     
-     * or *
-     
-     table.dataSource = sql.arr("select top 10 prenom, nom from users").toHashMap
-     then, arr("prenom") --> "Jon"
-     
+     * returns a non-null date (default '31/12/1999 23:59:59' )
      */
-    //public static void Arr(String strSQL) {
+    public static Timestamp Dat(String strSQL) throws ParseException {
+        String timeDefault = "31/12/1999 23:59:59";
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+        java.util.Date date = sdf.parse(timeDefault);
+        Timestamp result = new Timestamp(date.getTime());
+
+        Statement stmt;
+        ResultSet rs;
+
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeQuery(strSQL);
+            rs = stmt.getResultSet();
+            if (rs.next()) {
+                result = rs.getTimestamp(1);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /*
+     * returns an array ( recordset -> array )
+     */
+    /*
+     * table = new table() table.dataSource = sql.arr("select top 10 prenom, nom
+     * from users")
+     *
+     * or *
+     *
+     * table.dataSource = sql.arr("select top 10 prenom, nom from
+     * users").toHashMap then, arr("prenom") --> "Jon"
+     *
+     */
+    public static ArrayList<HashMap> Arr(String strSQL) {
+        Statement stmt;
+        ResultSet rs;
+        ArrayList list = new ArrayList<HashMap>();
+        int columnCount;
+
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeQuery(strSQL);
+            System.out.println(strSQL);
+
+            rs = stmt.getResultSet();
+            ResultSetMetaData md = rs.getMetaData();
+            columnCount = md.getColumnCount();
+            while (rs.next()) {
+                HashMap row = new HashMap(columnCount);
+                for (int i = 1; i <= columnCount; ++i) {
+                    row.put(md.getColumnName(i), rs.getObject(i));
+                }
+                list.add(row);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
     
+    /* return a container for table*/
+    /*public static void ContainerArr(String strSQL) {
+        Statement stmt;
+        ResultSet rs;
+        ArrayList list = new ArrayList<HashMap>();
+        int columnCount;
+        
+        Container container;
+        
+
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeQuery(strSQL);
+            System.out.println(strSQL);
+
+            rs = stmt.getResultSet();
+            ResultSetMetaData md = rs.getMetaData();
+            columnCount = md.getColumnCount();
+            while (rs.next()) {
+                HashMap row = new HashMap(columnCount);
+                for (int i = 1; i <= columnCount; ++i) {
+                    row.put(md.getColumnName(i), rs.getObject(i));
+                }
+                BeanItem item=new BeanItem(row);
+                list.add(row);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
+
     /* returns a vector ( 1 column recordset -> vector ) */
     /*
      for each value in sql.vec("select distinct mail from users")
         do something with the email
      next
      */
-    //public static void Vec(String strSQL) {
+    public static ArrayList Vec(String strSQL) {
+        Statement stmt;
+        ResultSet rs;
+        ArrayList list = new ArrayList();
+
+        try {
+            stmt = getConnection().createStatement();
+            stmt.executeQuery(strSQL);
+            System.out.println(strSQL);
+
+            rs = stmt.getResultSet();
+            while (rs.next()) {
+                list.add(rs.getObject(1));
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
     
 }
